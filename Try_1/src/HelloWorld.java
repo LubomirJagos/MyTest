0b10261e7dd527369621076f7ad9d30aa541c38b
jagoslub <jagoslub@WL313958.eu.tieto.com>
2016-09-01 14:41:25 +0200

    ServerClient aplikacia.
    SERVER časť.

---
+++ Try_1/src/HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+	public static void main(String args[]){
+		System.out.println("Hell o World!");
+		for( int i = 0; i < 100; i += 3){
+			System.out.print( i + " ");
+		}
+		System.out.print(" Konec.");
+	}
+}

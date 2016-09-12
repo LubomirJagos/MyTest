305f725678197aec200a823394fa8c4fed88b221
Lubomir Jagos <lubomir.jagos@tieto.com>
2016-09-12 12:41:53 +0200

    Added running JMockit test to show example for Expectations.
    Example is 'Garage' for repairing cars. When pass car into Garage object it change its engine with the same motor. During mocking there is change in engine type so test result would be negative.

--- JMockit_GarageTest/src/GarageTest.java
+++ JMockit_GarageTest/src/GarageTest.java
@@ -1,8 +1,10 @@
 import static org.junit.Assert.*;
 
 import mockit.Expectations;
-import mockit.E
-
+import mockit.NonStrictExpectations;
+import mockit.MockUp;
+import mockit.Mocked;
+import mockit.Mock;
 import org.junit.Test;
 
 public class GarageTest {
@@ -16,27 +18,49 @@ public class GarageTest {
     assertNotSame(expectedCar.engine, expectedEngine);
   }
 
+  /*	
+   *	Method EngineFactory.instantiate(...) is going to be mocked so it has to be annotate in parameters!
+   */
   @Test
-  public void testRepairWithExpectationEngineFactory() throws InstantiationException, IllegalAccessException {
-    final Engine expectedEngine = new DieselEngine();
+  public void testRepairWithExpectationEngineFactory(@Mocked final EngineFactory mock) throws InstantiationException, IllegalAccessException {
+
+	final Engine expectedEngine = new DieselEngine();
     final Engine actualEngine = new GasolineEngine();
     final Garage g = new CheapGarage();
     final Car expectedCar = new Car(actualEngine);
-    // here it is still the Engine built into the car
-    assertSame(expectedCar.engine, actualEngine);		//engine == gasoline
+    
+	// here it is still the Engine built into the car
+    assertSame(expectedCar.engine, actualEngine);
 
-    new Expectations() {
-      EngineFactory mock;
-      {
-        mock.instantiate(actualEngine); returns(new DieselEngine());		//change engine diesel instead gasoline
-      }
-    };
+    new Expectations(){
+    	{
+    		mock.instantiate(actualEngine);						//used method instantiate() to record that this method is mocked
+        	
+      		result = new DieselEngine();
+      		//result = new GasolineEngine();						//overriding return value of mocked method
+        }
+      };
+
+      //BEFORE repair
+      System.out.println("Engine type BEFORE repair");
+      System.out.println("-------------------------");
+      System.out.println("Engine type, gasoline: " + (expectedCar.engine instanceof GasolineEngine));
+	  System.out.println("Engine type, diesel: " + (expectedCar.engine instanceof DieselEngine));
 
     Car actualCar = g.repairEngine(expectedCar);
     assertSame(expectedCar, actualCar);
+
     // normally this will be a GasolineEngine, because that was built into the car
     // but because of the mocked EngineFactory, we get a DieselEngine instead
-    assertTrue(expectedCar.engine instanceof DieselEngine);    
+    
+    //AFTER repair
+    System.out.println();
+    System.out.println("Engine type AFTER repair");
+    System.out.println("-------------------------");
+    System.out.println("Engine type, gasoline: " + (expectedCar.engine instanceof GasolineEngine));
+    System.out.println("Engine type, diesel: " + (expectedCar.engine instanceof DieselEngine));
+    
+    assertTrue(expectedCar.engine instanceof DieselEngine);
     assertNotSame(expectedCar.engine, expectedEngine);
   }
 }
